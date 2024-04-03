47515bd0340c6498581a790572f89e171826df6b
Abhi9125 <abhi.singh3132@gmail.com>
2024-04-03 22:13:28 +0530

    Complete assignment

---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1 className="m-4 font-bold text-5xl">
+        We deliver food to your doorstep.
+      </h1>
+    </div>
+  );
+};
+
+export default About;
